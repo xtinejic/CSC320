19fa87231ec0e45cdd842d76a184d44329b709ff
Christine Cox <christinejoy.cox@gmail.com>
2025-01-19 23:22:39 -0500

    Initial Commit.

---
+++ src/module1/Person.java
@@ -0,0 +1,22 @@
+package module1;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstname = "Anna";
+		String lastname = "Smith";
+		String streetaddress = "456 Easy Street";
+		String city = "Salem";
+		String zipcode = "56789";
+		
+		// Person's information 
+		System.out.println("First Name: " + firstname);
+		System.out.println("Last Name: " + lastname);
+		System.out.println("Street Address: " + streetaddress);
+		System.out.println("City: " + city);
+		System.out.println("Zip code: " + zipcode);
+	}
+
+}
+ 
